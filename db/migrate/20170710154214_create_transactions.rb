b5bcec0b4ace23ec12bc54965bfc187e12622058
“rogrenke” <roland@rolandgrenke.com>
2017-07-10 16:46:47 +0100

    Creates migration for new transaction model

---
+++ db/migrate/20170710154214_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :transactions do |t|
+      t.float :amount, null: false
+      t.float :balance_after_transaction, null: false
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamp null: false
+    end
+  end
+end
